e12aff492a0ab4eece38a79b46d13452642a3c94
Harshit jain <harshit2020cs198@abesit.edu.in>
2022-12-26 22:11:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/passportuifinal/Passport_Power_Activity.java
@@ -0,0 +1,14 @@
+package com.example.passportuifinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Passport_Power_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passport_power);
+    }
+}
